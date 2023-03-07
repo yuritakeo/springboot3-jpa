ae77981e86d4ec18d55bd224045420bed74adbfa
Yuri Miyazaki <yuritakeo@ucl.br>
2023-03-07 00:05:50 -0300

    Service layer, component registration

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -12,15 +12,12 @@ import java.util.Arrays;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner { //CommandLineRunner = somente para iniciar a configuração?
-
     @Autowired
     private UserRepository userRepository;
-
     @Override
     public void run(String... args) throws Exception {
-
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        User u1 = new User(null, "Yuri Takeo Miyazaki", "yuritakeo@gmail.com", "27988792730", "123456");
+        User u2 = new User(null, "Fulano", "fulano@gmail.com", "11984563227", "654321");
 
         userRepository.saveAll(Arrays.asList(u1,u2));
     }
---
+++ src/main/java/com/example/course/services/UserService.java
@@ -0,0 +1,23 @@
+package com.example.course.services;
+
+import com.example.course.entitie.User;
+import com.example.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        return repository.findById(id).get();
+    }
+}
