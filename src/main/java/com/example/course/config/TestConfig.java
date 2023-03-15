37e95c73adce2c878f8f32d9ca00846fed2a1407
Yuri Miyazaki <yuritakeo@ucl.br>
2023-03-15 01:29:05 -0300

    Payment, one-to-one association

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -69,5 +69,10 @@ public class TestConfig implements CommandLineRunner { //CommandLineRunner = som
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+        Payment pay1 = new Payment(null, Instant.parse("2023-02-20T20:53:07Z"),o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
+
     }
 }
