39d66f02b328e62d5163fc34f8d9c04cc13da1d1
Yuri Miyazaki <yuritakeo@ucl.br>
2023-03-15 05:48:35 -0300

    Exception handling - update

--- src/main/java/com/example/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/course/services/exceptions/ResourceNotFoundException.java
@@ -5,7 +5,7 @@ public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(Object id){
-        super("Resource not found " + id);
+        super("Resource not found id: " + id);
     }
 
 }
